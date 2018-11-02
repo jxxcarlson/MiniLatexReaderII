8403b48801727417cea156adff57202338d70295
James Carlson <jxxcarlson@mac.com>
2018-11-02 17:43:16 -0400

    Use published MiniLatex package.

--- make.sh
+++ make.sh
@@ -3,7 +3,7 @@ reset=`tput setaf 7`
 
 echo
 echo "${color}Compiling ...${reset}"
-elm make --debug src/Main.elm --output=Main.js
+elm make src/Main.elm --output=Main.js
 
-# echo "${color}Minifiying ...${reset}"
-# uglifyjs Main.js -mc 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9"' -o Main.min.js
+echo "${color}Minifiying ...${reset}"
+uglifyjs Main.js -mc 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9"' -o Main.min.js
